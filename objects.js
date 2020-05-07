ccf752eff66dcddfb2a8cb35ef903bca993406dc
FS_link <mowglysofiann@gmail.com>
2020-05-07 02:54:10 +0200

    #3 - Updating Properties

--- objects.js
+++ objects.js
@@ -9,4 +9,26 @@ const userOne = {
     }
 };
 
+const userTwo = {
+    name: 'Yoshi',
+    email: 'yoshi@gmail.com',
+    login(){
+        console.log(this.email + ' You loggin !');
+    },
+    logout(){
+        console.log(this.email + ' You loggout !');
+    }
+};
+
+const userThree = {
+    name: 'Ryu',
+    email: 'Ryu@gmail.com',
+    login(){
+        console.log(this.email + ' You loggin !');
+    },
+    logout(){
+        console.log(this.email + ' You loggout !');
+    }
+};
+
 console.log(userOne);
