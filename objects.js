69cf829d423d62d30e7d0f01f576af93d7b34c40
FS_link <mowglysofiann@gmail.com>
2020-05-08 03:12:05 +0200

    #4 - Classes

--- objects.js
+++ objects.js
@@ -1,34 +1,3 @@
-const userOne = {
-    name: 'Yoshi',
-    email: 'yoshi@gmail.com',
-    login(){
-        console.log(this.email + ' You loggin !');
-    },
-    logout(){
-        console.log(this.email + ' You loggout !');
-    }
-};
-
-const userTwo = {
-    name: 'Yoshi',
-    email: 'yoshi@gmail.com',
-    login(){
-        console.log(this.email + ' You loggin !');
-    },
-    logout(){
-        console.log(this.email + ' You loggout !');
-    }
-};
-
-const userThree = {
-    name: 'Ryu',
-    email: 'Ryu@gmail.com',
-    login(){
-        console.log(this.email + ' You loggin !');
-    },
-    logout(){
-        console.log(this.email + ' You loggout !');
-    }
-};
-
-console.log(userOne);
+class User {
+    
+}
